581d9483607adb47be3dcbc2baf0e3e5ba208d81
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-10-24 17:13:14 -0700

    server.js modified with initializations

--- server.js
+++ server.js
@@ -11,5 +11,35 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 //Models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+const Note = require("./models/Note.js");
+const Article = require("./models/Article.js");
+
+//Port
+const PORT = process.env.PORT || 3030
+
+// Initialize Express
+const app = express();
+
+
+// Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/news-scraper");
+
+// Express Initialization
+const app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Make public a static directory
+app.use(express.static("public"));
