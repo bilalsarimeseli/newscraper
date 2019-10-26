df565d1bf213fd9479ea1536e307807b0372853a
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-10-26 09:50:31 -0700

    functions to save/delete articles added

--- public/app.js
+++ public/app.js
@@ -8,3 +8,25 @@ $("#scrape-btn").on("click", function() {
         window.location = "/"
     })
 });
+
+//SAVE ARTICLE
+$(".save").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
+    })
+});
+
+//DELETE ARTICLE
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/saved"
+    })
+});
